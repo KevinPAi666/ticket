fc169cd46013edef000ebdcb6270596d11e8c11a
KevinPAi666 <a0927989596@gmail.com>
2024-07-17 18:18:38 +0800

    add docker file

--- ticket/settings.py
+++ ticket/settings.py
@@ -74,14 +74,26 @@ WSGI_APPLICATION = 'ticket.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
+import pymysql
+pymysql.install_as_MySQLdb()
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'ticket',
+            'USER': 'root',
+            'PASSWORD': 'password',
+            'HOST': 'db',
+            'PORT': '3306',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
